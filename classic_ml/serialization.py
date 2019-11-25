e55d0ae7d1c30f3a05bbbcee3f478a3bd71663b7
Andrej Janchevski <janchevski.andrej@gmail.com>
2019-11-26 00:29:47 +0100

    Add compression

--- classic_ml/serialization.py
+++ classic_ml/serialization.py
@@ -1,14 +1,21 @@
 import _pickle as c_pickle
+import compress_pickle
 
 
-def save_object(obj, filepath):
+def save_object(obj, filepath, compress=True):
     with open(filepath, 'wb') as file:
-        c_pickle.dump(obj, file)
+        if compress:
+            compress_pickle.dump(obj, file, compression="gzip", set_default_extension=True)
+        else:
+            c_pickle.dump(obj, file)
         file.close()
 
 
-def load_object(filepath):
+def load_object(filepath, compressed=True):
     with open(filepath, 'rb') as file:
-        obj = c_pickle.load(file)
+        if compressed:
+            obj = compress_pickle.load(filepath)
+        else:
+            obj = c_pickle.load(file)
         file.close()
         return obj
