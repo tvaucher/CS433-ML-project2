9e76623f9c90d9dab0727bfd567f28d09a10386d
Andrej Janchevski <janchevski.andrej@gmail.com>
2019-11-26 00:28:20 +0100

    Modify feature selection to return objects reusable for test set

--- classic_ml/feature_selection.py
+++ classic_ml/feature_selection.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.feature_selection import SelectKBest, chi2, SelectFromModel
 from sklearn.ensemble import RandomForestClassifier
@@ -14,26 +12,23 @@ random_forest_model = RandomForestClassifier(
     n_jobs=-1)
 
 
-def normalize_dataset(dataset):
+def get_dataset_normalizer(dataset):
     scaler = MinMaxScaler()
-    return scaler.fit_transform(dataset)
+    scaler.fit(dataset)
+    return scaler
 
 
-def select_features_correlation_score(dataset, classes, feature_labels, num_features=30):
-    dataset = normalize_dataset(dataset)
+def get_feature_selector_correlation_score(dataset, classes, feature_labels, num_features=30):
     selector = SelectKBest(chi2, num_features)
-    dataset_reduced = selector.fit_transform(dataset, classes)
-    del dataset
-    best_features_scores = {feature_labels[index]: selector.scores_[index]
-                            for index in selector.get_support(indices=True)}
-    return pd.DataFrame(dataset_reduced, columns=list(best_features_scores.keys())), best_features_scores
+    selector.fit(dataset, classes)
+    features_scores = {feature_labels[index]: selector.scores_[index]
+                       for index in selector.get_support(indices=True)}
+    return selector, features_scores
 
 
-def select_features_information_gain(dataset, classes, feature_labels, num_features=30):
-    dataset = normalize_dataset(dataset)
+def get_feature_selector_information_gain(dataset, classes, feature_labels, num_features=30):
     selector = SelectFromModel(random_forest_model, max_features=num_features)
-    dataset_reduced = selector.fit_transform(dataset, classes)
-    del dataset
-    best_features_scores = {feature_labels[index]: selector.estimator_.feature_importances_[index]
-                            for index in selector.get_support(indices=True)}
-    return pd.DataFrame(dataset_reduced, columns=list(best_features_scores.keys())), best_features_scores
+    selector.fit(dataset, classes)
+    features_scores = {feature_labels[index]: selector.estimator_.feature_importances_[index]
+                       for index in selector.get_support(indices=True)}
+    return selector, features_scores
