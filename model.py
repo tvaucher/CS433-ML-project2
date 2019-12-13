45eaf93a2bf648c2dc4fd50997dab14ae38329f2
Timoté Vaucher <timote.vaucher@epfl.ch>
2019-12-13 21:28:42 +0100

    Add MultiLayerGRU model

--- model.py
+++ model.py
@@ -32,18 +32,34 @@ class BaselineGRU(nn.Module):
         self.hidden_dim_gru = 256
         self.embedding_dim, self.bidirectional, self.device = embedding_dim, bidirectional, device
         self.embedding = nn.Embedding.from_pretrained(vocab_vec)
-        self.gru = nn.GRU(self.embedding_dim,
-                          self.hidden_dim_gru, bidirectional=bidirectional)
+        self.gru = nn.GRU(self.embedding_dim, self.hidden_dim_gru, bidirectional=bidirectional, dropout=0.25)
         self.classifier = nn.Sequential(nn.Linear(self.hidden_dim_gru*2, 512), nn.ReLU(), nn.Dropout(),
                                         nn.Linear(512, 256), nn.ReLU(), nn.Dropout(),
                                         nn.Linear(256, 256), nn.ReLU(), nn.Dropout(),
                                         nn.Linear(256, 2), nn.LogSoftmax(dim=1))
-        #self.h0 = nn.Parameter(torch.zeros(2, 512, self.hidden_dim_gru).type(torch.FloatTensor), requires_grad=True)
-
+        h0 = torch.zeros(2, 1, self.hidden_dim_gru, dtype=torch.float).to(device)
+        self.h0 = nn.Parameter(h0, requires_grad=True)
+    
     def forward(self, seq, lengths):
-        embeddings = pack_padded_sequence(
-            self.embedding(seq), lengths).to(self.device)
-        h0 = torch.zeros((2, seq.size(1), self.hidden_dim_gru),
-                         dtype=torch.float).to(self.device)
-        _, h = self.gru(embeddings, h0)
+        embeddings = pack_padded_sequence(self.embedding(seq), lengths).to(self.device)
+        _, h = self.gru(embeddings, self.h0.repeat(1, seq.size(1), 1))
         return self.classifier(h.transpose(0, 1).reshape(-1, self.hidden_dim_gru * 2))
+
+class MultiLayerGRU(nn.Module):
+    def __init__(self, embedding_dim, vocab_vec, device, bidirectional=True, layers=3):
+        super().__init__()
+        self.hidden_dim_gru = 256
+        self.embedding_dim, self.bidirectional, self.layers, self.device = embedding_dim, bidirectional, layers, device
+        self.embedding = nn.Embedding.from_pretrained(vocab_vec)
+        self.gru = nn.GRU(self.embedding_dim, self.hidden_dim_gru, num_layers=layers, bidirectional=bidirectional, dropout=0.5)
+        self.classifier = nn.Sequential(nn.Linear(self.hidden_dim_gru, 512), nn.ReLU(), nn.Dropout(),
+                                        nn.Linear(512, 256), nn.ReLU(), nn.Dropout(),
+                                        nn.Linear(256, 256), nn.ReLU(), nn.Dropout(),
+                                        nn.Linear(256, 2), nn.LogSoftmax(dim=1))
+        h0 = torch.zeros(2*self.layers, 1, self.hidden_dim_gru, dtype=torch.float).to(self.device)
+        self.h0 = nn.Parameter(h0, requires_grad=True)
+    
+    def forward(self, seq, lengths):
+        embeddings = pack_padded_sequence(self.embedding(seq), lengths).to(self.device)
+        _, h = self.gru(embeddings, self.h0.repeat(1, seq.size(1), 1))
+        return self.classifier(h.sum(0).squeeze())
